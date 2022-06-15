3be312a322a3da60db2f5584b9de0e80002ee5e8
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-06-15 15:30:14 +0300

    adds task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
 
+const bodyEl = document.querySelector("body");
+const buttonStart = document.querySelector("button[data-start]");
+const buttonStop = document.querySelector("button[data-stop]");
+let timerId = null;
+
+buttonStop.disabled = true;
+
+buttonStart.addEventListener('click', runColor);
+buttonStop.addEventListener('click', stopColor);
+
+function runColor(){
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+	timerId = setInterval(colorChange, 1000);
+};
+
+function stopColor(){
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+    clearInterval(timerId);
+};
+
+//Функція, яка змінює колір фону <body>
+function colorChange(){
+    const color = getRandomHexColor();
+    bodyEl.style.backgroundColor = color;
+};
+
+//Функціця, яка генерує випадковий колір
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
