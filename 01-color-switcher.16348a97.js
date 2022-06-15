989a30e66156973f284544bc0d5ca013b398ef4e
MaximIvE <93542875+MaximIvE@users.noreply.github.com>
2022-06-15 12:31:30 +0000

    Deploying to gh-pages from @ MaximIvE/goit-js-hw-09@3be312a322a3da60db2f5584b9de0e80002ee5e8 🚀

---
+++ 01-color-switcher.16348a97.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),a=null;function d(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16));t.style.backgroundColor=e}n.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,n.disabled=!1,a=setInterval(d,1e3)})),n.addEventListener("click",(function(){e.disabled=!1,n.disabled=!0,clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.16348a97.js.map
