0f05d4a2b0d65dbe9e245ef8f28f5d433c559d18
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-06-15 22:51:44 +0300

    adds a fix to task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,12 +1,13 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
-// const inputEl = document.querySelector('#datetime-picker');
+const inputEl = document.querySelector('#datetime-picker');
 const btnStart = document.querySelector('button[data-start]');
 const NumbersEl = document.querySelectorAll('.value');
 
 let timeEnd = new Date().getTime() + 1000*(25+17*60+15*3600+18*3600*24);
 let timeStart = 0;
+let timerId = 0;
 
 btnStart.disabled = true;
 const options = {
@@ -19,36 +20,39 @@ const options = {
         timeStart = new Date().getTime();
         if (timeEnd > timeStart){
             btnStart.disabled = false;
+            inputEl.disabled = true;
         }else{
             alert('Please choose a date in the future');
             return;
         }
     },
 };
+
 flatpickr('#datetime-picker', options);
 
 btnStart.disabled = true;
-
-
-
-
-
-
 
 btnStart.addEventListener('click', runTimer);
 
 //Функція запускає таймер
 function runTimer(){
     btnStart.disabled = true;
-    const timerId = setInterval(callBackTime, 1000);
+    timerId = setInterval(callBackTime, 1000);
 };
 
 //Функція запускає повторення і запускає інші допоміжні функції
 function callBackTime(){
     timeStart = new Date().getTime();
+    if (timeEnd <= timeStart){
+        alert('Action!!!');
+        clearInterval(timerId);
+        inputEl.disabled = false;
+        return;
+    };
     const time = timeEnd - timeStart;
     const timeObj = pad(convertMs(time));
     drawsTinme(timeObj);
+    
 };
 
 // Функція перетворює мілісекунди в кількість секунд, хвилин, годин, днів, повертає об'єкт чисел
@@ -68,7 +72,7 @@ function convertMs(ms){
 // Функція приводить до формату xx, повертає об'єкт строк
 function pad(value){
     const result = {};
-    for (key in value){
+    for (const key in value){
         result[key] = String(value[key]).padStart(2, '0');
     };
     return result;
@@ -77,7 +81,7 @@ function pad(value){
 //Функція відмальовує об'єкт строк у відповідних полях на екрані
 function drawsTinme(timeObj){
     let i = 0;
-    for (key in timeObj){
+    for (const key in timeObj){
         NumbersEl[i].textContent = timeObj[key];
         i +=1;
     };
